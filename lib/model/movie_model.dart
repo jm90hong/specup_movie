4574e1625ef8075f81b35debec304b83e416e28b
jm90hong <jm90.hong@gmail.com>
2023-08-20 12:53:47 +0900

    12

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -1,16 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../vo/movie.dart';
+
 class MovieModel extends ChangeNotifier{
 
-  void loadMovies() async{
-    Uri url = Uri.parse('http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101');
+  List<Movie> movies = [];
+
+
+  void loadMovies({required String date}) async{
+    movies=[];
+    Uri url = Uri.parse('http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json');
     var requestUrlWithParam = url.replace(queryParameters:{
-      'b1_like_idx':''.toString(),
-      'board_idx':''.toString(),
+      'key':'f5eef3421c602c6cb7ea224104795888'.toString(),
+      'targetDt':date.toString(),
     });
     var response = await http.get(requestUrlWithParam);
-    print(response.body);
+    var jsonList = jsonDecode(response.body);
+    for(int i=0;i<jsonList.length;i++){
+      Movie mv = Movie.fromJson(jsonList[i]);
+      movies.add(mv);
+    }
+    notifyListeners();
   }
 
 
---
+++ lib/screen/movie_list_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MovieListScreen extends StatefulWidget {
+  const MovieListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MovieListScreen> createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends State<MovieListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
