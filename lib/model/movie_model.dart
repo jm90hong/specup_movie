437b6d6c42bbf0adeaecdcafb836f7c5623bde8c
jm90hong <jm90.hong@gmail.com>
2023-08-20 12:35:15 +0900

    12

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -4,8 +4,12 @@ import 'package:http/http.dart' as http;
 class MovieModel extends ChangeNotifier{
 
   void loadMovies() async{
-    Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    var response = await http.get(url);
+    Uri url = Uri.parse('http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101');
+    var requestUrlWithParam = url.replace(queryParameters:{
+      'b1_like_idx':''.toString(),
+      'board_idx':''.toString(),
+    });
+    var response = await http.get(requestUrlWithParam);
     print(response.body);
   }
 
